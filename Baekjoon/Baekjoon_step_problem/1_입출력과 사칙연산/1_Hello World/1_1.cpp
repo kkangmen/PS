eda71cee9974920ed6086292766c1c6e2f053214
kkangmen <pickofee@gmail.com>
2023-11-16 14:13:06 +0900

    11/16

--- Baekjoon/Baekjoon_step_problem/1_입출력과 사칙연산/1_Hello World/1_1.cpp
+++ Baekjoon/Baekjoon_step_problem/1_입출력과 사칙연산/1_Hello World/1_1.cpp
@@ -1,13 +1,10 @@
-/*
-문제
- -Hello World!를 출력하시오.
-입력
- -없음
-출력
- -Hello World!를 출력하시오.
-예제 입력 1
- -없음
-예제 출력 1
- -Hello World!
-https://www.acmicpc.net//problem/2557
-*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello World!" << endl;
+    cout << "123" << endl;
+    return 0;
+}
