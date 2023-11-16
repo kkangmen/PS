ff5c1fd113a99057946a8796799b7d5a5c90a700
kkangmen <pickofee@gmail.com>
2023-11-16 14:41:24 +0900

    11/16

--- Baekjoon/Baekjoon_step_problem/5_1차원 배열/1_최소, 최대/5_1.cpp
+++ Baekjoon/Baekjoon_step_problem/5_1차원 배열/1_최소, 최대/5_1.cpp
@@ -1,16 +1,31 @@
-/*
-문제
- -N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
-입력
- -첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
-출력
- -첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
-예제 입력 1
- -5
-20 10 35 30 7
+#include <iostream>
 
-예제 출력 1
- -7 35
+using namespace std;
 
-https://www.acmicpc.net//problem/10818
-*/
+int main(void){
+    int num_of_int; // ������ ����
+    cin >> num_of_int; // ������ ���� �Է�
+    int num_array[num_of_int]; // ������ ������ ���̸�ŭ �迭 ����
+
+    for (int i = 0; i < num_of_int; i++){
+        cin >> num_array[i]; // �迭�� ������ ����
+    }
+
+    int max = num_array[0]; // �ִ�
+    int min = num_array[0]; // �ּڰ�
+
+    for (int i = 1; i < num_of_int; i++){ // �ּڰ��� ���ϴ� �˰�����
+        if (min > num_array[i]){
+            min = num_array[i];
+        }
+    }
+
+    for (int i = 1; i < num_of_int; i++){ // �ִ��� ���ϴ� �˰�����
+        if (max < num_array[i]){
+            max = num_array[i];
+        }
+    }
+    cout << min << " " << max << endl;
+
+    return 0;
+}
