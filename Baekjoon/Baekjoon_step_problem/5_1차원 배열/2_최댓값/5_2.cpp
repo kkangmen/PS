93d2303ec36972133ff1414d0e9da8f6a77061ae
kkangmen <pickofee@gmail.com>
2023-11-17 16:31:54 +0900

    11/17

--- Baekjoon/Baekjoon_step_problem/5_1차원 배열/2_최댓값/5_2.cpp
+++ Baekjoon/Baekjoon_step_problem/5_1차원 배열/2_최댓값/5_2.cpp
@@ -10,9 +10,9 @@ int main(){
         cin >> num_array[i];
     } // 배열에 숫자 대입
 
-    int max_index; // 최댓값의 인덱스를 저장할 변수
-    int max = num_array[0]; // max를 num_array의 0번째인덱스로 지정
-    for (int i = 1; i < num_array.size(); i++){
+    int max_index = 0; // 최댓값의 인덱스를 저장할 변수
+    int max = 0; // max를 num_array의 0번째인덱스로 지정
+    for (int i = 0; i < num_array.size(); i++){
         if (max < num_array[i]){// max값보다 큰값이 있을 경우 최신화
             max = num_array[i];
             max_index = i; // 최댓값의 익덱스를 저장
