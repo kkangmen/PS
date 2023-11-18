6271e7b01c69630b838c1082deb3fc53632a8b0b
kkangmen <pickofee@gmail.com>
2023-11-18 17:18:27 +0900

    11/18

--- Baekjoon/Baekjoon_step_problem/5_1차원 배열/6_OX퀴즈/5_6.cpp
+++ Baekjoon/Baekjoon_step_problem/5_1차원 배열/6_OX퀴즈/5_6.cpp
@@ -1,26 +1,34 @@
-/*
-문제
- -"OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
-"OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
-OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
-입력
- -첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 길이가 0보다 크고 80보다 작은 문자열이 주어진다. 문자열은 O와 X만으로 이루어져 있다.
-출력
- -각 테스트 케이스마다 점수를 출력한다.
-예제 입력 1
- -5
-OOXXOXXOOO
-OOXXOOXXOO
-OXOXOXOXOXOXOX
-OOOOOOOOOO
-OOOOXOOOOXOOOOX
+#include <iostream>
+#include <string>
 
-예제 출력 1
- -10
-9
-7
-55
-30
+using namespace std;
 
-https://www.acmicpc.net//problem/8958
-*/
+int OX(string& s){
+
+    int x = 0;
+    int sum = 0;
+    for (char &v : s){
+        if (v == 'O'){
+            x++;
+            sum += x;
+        }
+        else{
+            x = 0;
+        }
+    }
+    return sum;
+}
+
+int main(){
+    int test_case;
+    cin >> test_case;
+
+    for (int i = 0; i < test_case; i++){
+        string s;
+        cin >> s;
+        
+        cout << OX(s) << endl;
+    }
+    return 0;
+}
+
--- Baekjoon/Baekjoon_step_problem/5_1차원 배열/7_평균은 넘겠지/5_7.cpp
+++ Baekjoon/Baekjoon_step_problem/5_1차원 배열/7_평균은 넘겠지/5_7.cpp
@@ -1,25 +1,48 @@
-/*
-문제
- -대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
-입력
- -첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
-둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
-출력
- -각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
-예제 입력 1
- -5
-5 50 50 70 80 100
-7 100 95 90 80 70 60 50
-3 70 90 80
-3 70 90 81
-9 100 99 98 97 96 95 94 93 91
+#include <iostream>
 
-예제 출력 1
- -40.000%
-57.143%
-33.333%
-66.667%
-55.556%
+using namespace std;
 
-https://www.acmicpc.net//problem/4344
-*/
+int avg(int[] , int);
+double develop_testcase(int);
+
+int main(){
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++){
+        cout.precision(5);
+        cout << develop_testcase(T) << "%" << endl;
+    }
+    return 0;
+}
+
+double develop_testcase(int T){
+    int per;
+    cin >> per;
+
+    int score_arr[per]= {};
+    for (int i = 0; i < per; i++){
+        cin >> score_arr[i];
+    }
+
+    int average = avg(score_arr, per);
+    //cout << "average: " << average << endl;
+
+    int count = 0;
+    for (int i = 0; i < per; i++){
+        if (average < score_arr[i]){
+            count++;
+        }
+    }
+
+    return static_cast<double>(count)/ per * 100;
+}
+
+int avg(int score_arr[], int per){
+    double sum = 0;
+    for (int i = 0; i < per; i++){
+        sum += score_arr[i];
+    }
+
+    return sum / per;
+}
