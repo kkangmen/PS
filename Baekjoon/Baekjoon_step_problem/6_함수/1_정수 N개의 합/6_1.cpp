4105654d2df50e283ced00e4724e444f99c2000e
kkangmen <pickofee@gmail.com>
2023-11-20 16:08:46 +0900

    11/20

--- Baekjoon/Baekjoon_step_problem/6_함수/1_정수 N개의 합/6_1.cpp
+++ Baekjoon/Baekjoon_step_problem/6_함수/1_정수 N개의 합/6_1.cpp
@@ -1,36 +1,29 @@
-/*
-문제
- -정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
-작성해야 하는 함수는 다음과 같다.
+#include <iostream>
+#include <vector>
 
-C, C11, C (Clang), C11 (Clang): long long sum(int *a, int n);
+using namespace std;
 
-a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
-n: 합을 구해야 하는 정수의 개수
-리턴값: a에 포함되어 있는 정수 n개의 합
+long long sum(vector<int> &a);
 
 
-C++, C++11, C++14, C++17, C++ (Clang), C++11 (Clang), C++14 (Clang), C++17 (Clang): long long sum(std::vector<int> &a);
+int main(){
+    vector<int> a(5); 
 
-a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
-리턴값: a에 포함되어 있는 정수 n개의 합
+    for (int i = 0; i < 5; i++){
+        cin >> a[i];
+    }
 
+    cout << sum(a) << endl;
 
-Python 2, Python 3, PyPy, PyPy3: def solve(a: list) -> int
+    return 0;
+}
 
-a: 합을 구해야 하는 정수 n개가 저장되어 있는 리스트 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
-리턴값: a에 포함되어 있는 정수 n개의 합 (정수)
+long long sum(vector<int> &a){
+    int a_size = a.size();
+    long long ans = 0;
+    for (int i = 0; i < a_size; i++){
+        ans += a[i];
+    }
 
-
-Java: long sum(int[] a); (클래스 이름: Test)
-	
-a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
-리턴값: a에 포함되어 있는 정수 n개의 합
-
-
-Go: sum(a []int) int
-
-a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
-리턴값: a에 포함되어 있는 정수 n개의 합
-https://www.acmicpc.net//problem/15596
-*/
+    return ans;
+}
