f5e888aaaf4c105a52b1ffd2f01e3e6b3d99e2cc
kkangmen <pickofee@gmail.com>
2023-11-21 14:59:43 +0900

    11/21

--- Baekjoon/Baekjoon_step_problem/6_함수/2_셀프 넘버/6_2.cpp
+++ Baekjoon/Baekjoon_step_problem/6_함수/2_셀프 넘버/6_2.cpp
@@ -3,45 +3,51 @@
 using namespace std;
 
 void d(int n);
-int num(int n);
+int add_num(int n);
+void get_num(bool PrimeArray[], int n);
 
 int main(){
     
-    cout << num(100);
+    d(10000);
 
     return 0;
 }
 
-// void d(int n){
+void d(int n){
 
-//     bool* PrimeArray = new bool[n+1];
+    bool* PrimeArray = new bool[n+1];
 
-//     for (int i = 1; i <= n; i++){
-//         PrimeArray[i] = true;
-//     }
+    for (int i = 1; i <= n; i++){
+        PrimeArray[i] = true;
+    }
 
-//     for (int i = 1; i * i <= n; i++){
-//         if (PrimeArray[i]){
-//             for (int j = i * i; j <= n; j += i){
-//                 PrimeArray[j] = false;
-//             }
-//         }
-//     }
+    for (int i = 1; i <= n; i++){
+        if (PrimeArray[i]){
+            get_num(PrimeArray, i);
+        }
+    }
 
-//     for (int i = 2; i <= n; i++){
-//         if (PrimeArray[i]){
-//             cout << i << endl;
-//         }
-//     }
-// }
+    for (int i = 1; i <= n; i++){
+        if (PrimeArray[i]){
+            cout << i << endl;
+        }
+    }
+}
 
-int num(int n){
+int add_num(int n){
     int sum = n;
 
-    while (n%10 == 0){
-        sum += n%10;
-        n = n/10;
+    while (n != 0){
+        sum += n % 10;
+        n /= 10;
     }
 
     return sum;
+}
+
+void get_num(bool PrimeArray[], int n){
+    while (add_num(n) <= 10000){
+        PrimeArray[add_num(n)] = false;
+        n = add_num(n);
+    }
 }
