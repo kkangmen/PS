7799ab20cdcf2e9c41cdf9579d8758461233e48f
kkangmen <pickofee@gmail.com>
2023-11-22 15:40:06 +0900

    11/22

--- Baekjoon/Baekjoon_step_problem/6_함수/3_한수/6_3.cpp
+++ Baekjoon/Baekjoon_step_problem/6_함수/3_한수/6_3.cpp
@@ -1,39 +1,48 @@
-/*
-문제
- -어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다. 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다. N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
-입력
- -첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
-출력
- -첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
-예제 입력 1
- -110
+#include <iostream>
 
-예제 출력 1
- -99
+using namespace std;
 
-예제 입력 2
- -1
+int put_num(int i);
+int check_num(int arr[]);
+int main(){
+    int N;
+    cin >> N;
+    int count = 0;
 
-예제 출력 2
- -1
+    for (int i = 1; i <= N; i++){
+        if(i < 100){
+            count++;
+        }
+        else if (i == 1000){}
+        else{
+           if (put_num(i) == 1){
+            count++;
+           }
+        }
+    }
+    cout << count << endl;
+  
+    return 0;
+}
 
-예제 입력 3
- -210
+int put_num(int i){
+    int arr[3] = {};
+    for (int j = 0; j < 3; j++){
+        if (i != 0){
+            arr[j] = i%10;
+            i /= 10;
+        }
+    }
 
-예제 출력 3
- -105
+    int result = check_num(arr);
+    return result;
+}
 
-예제 입력 4
- -1000
-
-예제 출력 4
- -144
-
-예제 입력 5
- -500
-
-예제 출력 5
- -119
-
-https://www.acmicpc.net//problem/1065
-*/
+int check_num(int arr[]){
+    if (arr[2] - arr[1] == arr[1] - arr[0]){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
