eafaa9ee7e6e72e73d1590518a7fad949d5f31f7
kkangmen <pickofee@gmail.com>
2023-11-28 17:00:40 +0900

    11/28

--- Beakjoon알골강의/스택/스택.cpp
+++ Beakjoon알골강의/스택/스택.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include <stack>
+#include <string>
 
 using namespace std;
 
 int main(){
-    cout << "Hello World" << endl;
-    cout << "Çï·Î¿ì ¿ùµå" << endl;
+    stack<int> stack;
+
+    string order;
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++){
+        cin >> order;
+        if (order == "push"){
+            int num;
+            cin >> num;
+            stack.push(num);
+        }
+        else if(order == "pop"){
+            if(stack.size() == 0){
+                cout << -1 << endl;
+            }
+            else{
+                cout << stack.top() << endl;
+                stack.pop();
+            }
+        }
+        else if (order == "size"){
+            cout << stack.size() << endl;
+        }
+        else if (order == "empty"){
+            if (stack.size() == 0){
+                cout << 1 << endl;
+            }
+            else {
+                cout << 0 << endl;
+            }
+        }
+        else if (order == "top"){
+            if (stack.size()== 0){
+                cout << -1 << endl;
+            }
+            else {
+                cout << stack.top() << endl;
+            }
+        }
+    }
     return 0;
 }
