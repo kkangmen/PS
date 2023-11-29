3a0194123e360754d9075268f98afbbeaa36aee3
kkangmen <pickofee@gmail.com>
2023-11-29 15:18:31 +0900

    11/29

--- Beakjoon알골강의/자료구조 1/단어 뒤집기.cpp
+++ Beakjoon알골강의/자료구조 1/단어 뒤집기.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <stack>
+#include <string>
 
 using namespace std;
 
 int main(){
-    stack<string> stack;
+   int testcase;
+   string sentence;
+   stack<char> stack;
 
-    string sentence;
-    cin >> sentence;
-    
-    int testcase;
-    cin >> testcase;
-    
-    for (char ch : sentence){
-        cout << ch << endl;
+   cin >> testcase;
+   cin.ignore();
+
+   while (testcase--){
+    getline(cin, sentence);
+    sentence += ' ';
+
+    for (int i = 0; i < sentence.size(); i++){
+        if(sentence[i] == ' '){
+            while (!stack.empty()){
+                cout << stack.top();
+                stack.pop();
+            }cout << " ";
+        }
+        else{
+            stack.push(sentence[i]);
+        }
     }
-
+    cout << endl;
+   }
 
     return 0;
 }
