05ccf5272d21d1c5bb83601810839f80499d712a
kkangmen <pickofee@gmail.com>
2023-12-10 14:52:12 +0900

    12/10

--- Beakjoon알골강의/다이나믹 프로그래밍1/카드 구매하기.cpp
+++ Beakjoon알골강의/다이나믹 프로그래밍1/카드 구매하기.cpp
@@ -4,25 +4,23 @@
 using namespace std;
 
 int main(){
-    // int n;
-    // cin >> n;
+    int n;
+    cin >> n;
 
-    int d[1001];
-    int p[1001];
+    int d[1000] = {0, };
+    int p[1000] = {0, };
 
-    cout << d[0];
-    
-    // for (int i = 1; i <= n; i++){
-    //     cin >> p[i];
-    // }
+    for (int i = 1; i <= n; i++){
+        cin >> p[i];
+    }
 
-    // for (int i = 1; i <= n; i++){
-    //     for (int j = 1; j <= i; j++){
-    //         d[i] = max(d[i], d[i-j] + p[j]);
-    //     }
-    // }
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+            d[i] = max(d[i], d[i-j] + p[j]);
+        }
+    }
 
-    // cout << d[n] << endl;
+    cout << d[n] << endl;
     
     return 0;
 }
