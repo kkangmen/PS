d17fb98d14155b6a483f7c5e7e5dabd26c2795f0
kkangmen <pickofee@gmail.com>
2024-01-08 16:51:24 +0900

    1/8

--- debug/debug.cpp
+++ debug/debug.cpp
@@ -2,15 +2,27 @@
 
 using namespace std;
 
+
 int main(){
-    int n;
+    string word;
+    char a[5][16];
+    
+    for (int i = 0; i < 5; i++){
+        cin >> word;
 
-    cin >> n;
-
-    for (int i = 1; i <= n/4; i++){
-        cout << "long ";
+        for (int j = 0; j < word.size(); j++){
+            a[i][j] = word[j];
+        }
     }
 
-    cout << "int" << '\n';
+    for (int i = 0; i < 15; i++){
+        for (int j = 0; j < 5; j++){
+            if (a[j][i] != NULL){
+                cout << a[j][i];
+            }
+        }
+    }
+
+    cout << '\n';
     return 0;
 }
