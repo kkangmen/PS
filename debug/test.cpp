d9f9f68afdae3d5a28270219411c2e5de150b55c
kkangmen <pickofee@gmail.com>
2024-07-03 17:08:09 +0900

    7/3

---
+++ debug/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int a = 0;
+    for (int i = 0; i < 5; i++){
+        a++;
+        cout << a << ' ';
+    }
+    return 0;
+}
