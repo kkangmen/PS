a67498d8f9c3f54aa1a261d4adc6fa6f84664351
kkangmen <pickofee@gmail.com>
2024-07-21 17:11:30 +0900

    .

--- 2024 ICPC Sinchon Winter Camp Novice/[4회차] Prefix Sum, Two Pointer/과일 탕후루.cpp
+++ 2024 ICPC Sinchon Winter Camp Novice/[4회차] Prefix Sum, Two Pointer/과일 탕후루.cpp
@@ -7,35 +7,41 @@ using namespace std;
 int a[200001]; // 과일의 개수 정렬
 int f_count[10];
 
-int main(){
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
+
     int n; // 과일의 개수 입력
     cin >> n;
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> a[i];
     }
 
-    int l = 0; 
-    int r = 0; 
+    int l = 0;
+    int r = 0;
     int len = 0;
     int kind = 0;
     int ans = 0;
-    while (r < n){
+    while (r < n)
+    {
         int cnt = 0;
 
-        if (f_count[a[r]] == 0){
+        if (f_count[a[r]] == 0)
+        {
             kind++;
         }
         len++;
         f_count[a[r]]++;
 
-        while (kind > 2){
+        while (kind > 2)
+        {
             len--;
             f_count[a[l]]--;
-            if (f_count[a[l]] == 0){
+            if (f_count[a[l]] == 0)
+            {
                 kind--;
             }
             l++;
