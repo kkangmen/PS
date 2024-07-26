1ba6bcf2150f3490428af8fc3fc2d41c66b29ef5
kkangmen <pickofee@gmail.com>
2024-07-26 18:15:42 +0900

    디벅

--- debug/debug.cpp
+++ debug/debug.cpp
@@ -3,33 +3,61 @@
 #include <vector>
 using namespace std;
 
+int n;
+int t[15];
+int p[15];
+int ans = 0;
+int num = 0;
+int go(int index, int ps)
+{
+    cout << "go(index,ps)호출: " << index << ' ' << ps << '\n';
+    if (index + t[index] > n)
+    {
+        cout << "ps값: " << ps - p[index] << '\n';
+        return ps - p[index];
+    }
+    if (index + t[index] == n)
+    {
+        cout << "ps값: " << ps << '\n';
+        return ps;
+    }
 
-int main() {
+    for (int i = index + t[index]; i < n; i++)
+    {
+        if (index + t[index] > n)
+        {
+            continue;
+        }
+        int val = go(index + t[index], ps + p[index + t[index]]);
+        // cout << "i =  " << i << ' ' << "val 값: " << val << '\n';
+        ans = max(ans, val);
+    }
+
+    return ans;
+}
+int main()
+{
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    int n;
     cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> t[i] >> p[i];
+    }
 
-    int sum = 0;
-    int ans = 0;
-    if (1 <= n < 10){
-        for (int i = 0; i < n; i++){
-            ans += 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i + t[i] > n)
+        {
+            continue;
         }
-        cout << ans << '\n';
+        cout << "go(i,p[i]) 호출: " << i << '\n'
+             << '\n';
+        num = max(go(i, p[i]), num);
+        // cout << "i = : " << i << "값: " << num << '\n';
     }
-    else if (10 <= n < 100){
-        for (int i = 10; i < n; i++){
-            ans += 2;
-        }
-        cout << 9 + ans << '\n';
-    }
-    else if (n < 1000){
-        for (int i = 100; i < n; i++){
-            ans += 3;
-        }
-        cout << 9 + 180 + ans << '\n';
-    }
+    // cout << num << '\n';
     return 0;
 }
