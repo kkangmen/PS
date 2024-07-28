51669e65286395a2d33b4cdd1cf4dde807a9dbdc
kkangmen <pickofee@gmail.com>
2024-07-28 18:10:04 +0900

    테스트

--- debug/debug.cpp
+++ debug/debug.cpp
@@ -4,37 +4,69 @@
 using namespace std;
 
 int n;
-int t[15];
-int p[15];
-int ans = 0;
-int num = 0;
-int go(int index, int ps)
+int s[21][21];
+vector<int> teamA;
+vector<int> teamB;
+int min_num = 1e9;
+
+int calculate(vector<int> &teamA, vector<int> &teamB)
 {
-    cout << "go(index,ps)호출: " << index << ' ' << ps << '\n';
-    if (index + t[index] > n)
+    int teamA_sum = 0;
+    int teamB_sum = 0;
+    if (teamA.size() > 1)
     {
-        cout << "ps값: " << ps - p[index] << '\n';
-        return ps - p[index];
-    }
-    if (index + t[index] == n)
-    {
-        cout << "ps값: " << ps << '\n';
-        return ps;
-    }
-
-    for (int i = index + t[index]; i < n; i++)
-    {
-        if (index + t[index] > n)
+        for (int i = 0; i < teamA.size() - 1; i++)
         {
-            continue;
+            for (int j = i + 1; j < teamA.size(); j++)
+            {
+                teamA_sum += s[i][j];
+                teamA_sum += s[j][i];
+            }
         }
-        int val = go(index + t[index], ps + p[index + t[index]]);
-        // cout << "i =  " << i << ' ' << "val 값: " << val << '\n';
-        ans = max(ans, val);
     }
-
+    if (teamB.size() > 1)
+    {
+        for (int i = 0; i < teamB.size() - 1; i++)
+        {
+            for (int j = i + 1; j < teamB.size(); j++)
+            {
+                teamB_sum += s[i][j];
+                teamB_sum += s[j][i];
+            }
+        }
+    }
+    int ans = abs(teamA_sum - teamB_sum);
     return ans;
 }
+
+void go(int index, char team)
+{
+    cout << "go(index, team) 호출: " << index << ' ' << team << '\n';
+    if (index == n)
+    {
+        if (teamA.size() > 0 && teamB.size() > 0)
+        {
+            int ans = calculate(teamA, teamB);
+            min_num = min(ans, min_num);
+        }
+        return;
+    }
+
+    if (team == 'a')
+    {
+        teamA.push_back(index);
+    }
+    else
+    {
+        teamB.push_back(index);
+    }
+
+    go(index + 1, 'a');
+    teamA.pop_back();
+    go(index + 1, 'b');
+    teamB.pop_back();
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -42,22 +74,18 @@ int main()
     cout.tie(NULL);
 
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        cin >> t[i] >> p[i];
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> s[i][j];
+        }
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        if (i + t[i] > n)
-        {
-            continue;
-        }
-        cout << "go(i,p[i]) 호출: " << i << '\n'
-             << '\n';
-        num = max(go(i, p[i]), num);
-        // cout << "i = : " << i << "값: " << num << '\n';
-    }
-    // cout << num << '\n';
+    go(1, 'a');
+    teamA.clear();
+    teamB.clear();
+    go(1, 'b');
+    // cout << min_num << '\n';
     return 0;
 }
