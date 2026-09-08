1cbb6a0bbbcb6fdabcbf30da7a08574cba13e467
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-08 12:09:44 +0900

    [level 2] Title: 삼각 달팽이, Time: 25.13 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,61 +1,36 @@
 import java.util.*;
 
 class Solution {
-    
-    int[][] triangle;
-    int num = 1;
-    int row = -1;
-    int col = 0;
-    int number = 1;
-    
-    public void goDown(int count){
-        for (int i = 0; i < count; i++){
-            row += 1;
-            triangle[row][col] = number;
-            number++;
-        }
-    }
-    
-    public void goRight(int count){
-        for (int i = 0; i < count; i++){
-            col += 1;
-            triangle[row][col] = number;
-            number++;
-        }
-    }
-        
-    public void goDiag(int count){
-        for (int i = 0; i < count; i++){
-            row -= 1;
-            col -= 1;
-            triangle[row][col] = number;
-            number++;
-        }
-    }
-    
-    public List<Integer> solution(int n) {
+    public int[] solution(int n) {
         List<Integer> answer = new ArrayList<>();
         
-        triangle = new int[n][n];
+        int[][] triangle = new int[n][n];
         
+        int number = 1;
+        int row = -1;
+        int col = 0;
         for (int i = 0; i < n; i++){
-            switch (i%3) {
-                case 0: goDown(n-i);
-                    break;
-                case 1: goRight(n-i);
-                    break;
-                case 2: goDiag(n-i);
-                    break;
+            for (int j = 0; j < n-i; j++){
+                if (i%3 == 0){
+                    row++;
+                } else if (i%3 == 1){
+                    col++;
+                } else {
+                    row--;
+                    col--;
+                }
+                
+                triangle[row][col] = number++;
             }
         }
         
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
                 if (triangle[i][j] != 0){
-                    answer.add(triangle[i][j]);                    
+                    answer.add(triangle[i][j]);
                 }
             }
         }
-        return answer;
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
