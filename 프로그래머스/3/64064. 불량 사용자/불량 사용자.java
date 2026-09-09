fa76ef9bb059f64d28148a273ef66ec04d1d1d92
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-09 10:59:19 +0900

    [level 3] Title: 불량 사용자, Time: 31.94 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -4,43 +4,40 @@ class Solution {
     
     boolean[] isUsed;
     Set<List<String>> set = new HashSet<>();
-    String[] cur_id;
     
-    public boolean isPossible(String id1, String id2){
-        if (id1.length() != id2.length()){
+    public boolean isPossible(String user_id, String banned_id){
+        if (user_id.length() != banned_id.length()){
             return false;
-        }
+        } 
         
-        for (int i = 0; i < id1.length(); i++){
-            char ch1 = id1.charAt(i);
-            char ch2 = id2.charAt(i);
+        for (int i = 0; i < user_id.length(); i++){
+            char user_id_char = user_id.charAt(i);
+            char banned_id_char = banned_id.charAt(i);
             
-            if (ch2 == '*'){
-                continue;
-            }
-            
-            if (ch1 != ch2){
+            // 글자가 같거나 둘중 하나라도 별이라면 참 -> 글자가 다르고, 별이 아니면 거짓
+            if (user_id_char != banned_id_char &&
+               banned_id_char != '*'){
                 return false;
             }
         }
-        
         return true;
     }
     
-    public void bTracking(String[] user_id, String[] banned_id, String[] cur_id, int length, int size){
-        if (size == length){
-            String[] copy = cur_id.clone();
-            Arrays.sort(copy);
-            set.add(new ArrayList<>(List.of(copy)));
+    public void bTracking(String[] user_id, String[] banned_id, List<String> cur_id){
+        if (cur_id.size() == banned_id.length){
+            List<String> copy = new ArrayList<>(cur_id);
+            Collections.sort(copy);
+            set.add(copy);
             return;
-        }
+        }    
         
         for (int i = 0; i < user_id.length; i++){
-            if (!isUsed[i] && isPossible(user_id[i], banned_id[size])){
-                cur_id[size] = user_id[i];
+            if (!isUsed[i] && isPossible(user_id[i], banned_id[cur_id.size()])){
                 isUsed[i] = true;
-                bTracking(user_id, banned_id, cur_id, length, size+1);
+                cur_id.add(user_id[i]);
+                bTracking(user_id, banned_id, cur_id);
                 isUsed[i] = false;
+                cur_id.remove(user_id[i]);
             }
         }
     }
@@ -48,14 +45,13 @@ class Solution {
     public int solution(String[] user_id, String[] banned_id) {
         
         isUsed = new boolean[user_id.length];
-        cur_id = new String[banned_id.length];
+        List<String> cur_id = new ArrayList<>();
+    
+        bTracking(user_id, banned_id, cur_id);
         
-        bTracking(user_id, banned_id, cur_id, banned_id.length, 0);
-        
-        // System.out.println("정답");
-        // for (List<String> s: set){
-        //     for (String str : s){
-        //         System.out.print(str + " ");
+        // for (List<String> list : set){
+        //     for (String s : list){
+        //         System.out.print(s + " ");
         //     }
         //     System.out.println();
         // }
