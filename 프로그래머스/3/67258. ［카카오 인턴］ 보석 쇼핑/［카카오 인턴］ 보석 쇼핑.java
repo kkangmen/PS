cd0d0aeacceedbe007a077354a5b717465204956
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-10 13:09:08 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 26.39 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,58 +1,51 @@
 import java.util.*;
 
 class Solution {
-    
-    Set<String> gemSet = new HashSet<>();
-    Map<String, Integer> map = new HashMap<>();
-    
     public int[] solution(String[] gems) {
-        int[] answer = new int[2];
+        int[] answer = new int[]{0, gems.length-1};
         
-        for (String s : gems){
-            gemSet.add(s);
+        Set<String> set = new HashSet<>();
+        
+        for(String gem : gems){
+            set.add(gem);
         }
-        int totalKind = gemSet.size();
         
-        // 슬라이딩 윈도우 시작
-        int startIdx = 0;
-        int endIdx = -1;
-        int minGap = gems.length+1;
-        while (startIdx < gems.length){
+        Map<String, Integer> map = new HashMap<>();
+        int start = 0;
+        int end = 0;
+        map.put(gems[0], 1);
+        
+        while (true){
             
-            // 만약 갯수가 부족하다면 endIdx 증가
-            if (map.size() < totalKind){
-                endIdx++;
-                if (endIdx == gems.length){
+            // 전체 가짓수 부족할 경우
+            if (map.size() < set.size()){
+                // System.out.println("부족");
+                end++;
+                if (end == gems.length){
                     break;
                 }
-                map.put(gems[endIdx], map.getOrDefault(gems[endIdx], 0) + 1);
-            }
-            // 갯수가 충분하면 startIdx 증가
-            else {
-                map.put(gems[startIdx], map.get(gems[startIdx]) -1);
-                
-                // 0이 되면 제거
-                if (map.get(gems[startIdx]) == 0){
-                    map.remove(gems[startIdx]);
+                map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);    
+            } else { // 전체 가짓수 충분
+                // System.out.println("충분");
+                if (end-start < answer[1]-answer[0]){
+                    answer[0] = start;
+                    answer[1] = end;
+                    // System.out.println(answer[0] + " " + answer[1]);
                 }
                 
-                startIdx++;
+                map.put(gems[start], map.get(gems[start]) -1);
+                if (map.get(gems[start]) == 0){
+                    map.remove(gems[start]);
+                }
+                start++;
             }
             
-            // System.out.println(startIdx + " " + endIdx);
-            // 최신화된 현재 갯수가 totalKind와 같을 경우
-            if (map.size() == totalKind){
-                // System.out.println("가능");
-                // 기존 길이보다 짧을 경우 정답 갱신
-                int curGap = endIdx-startIdx+1;
-                if (minGap > curGap){
-                    minGap = curGap;
-                    answer[0] = startIdx+1;
-                    answer[1] = endIdx+1;
-                }
-            }
+            // for (String s : map.keySet()){
+            //     // System.out.println("key: " + s + " value: " + map.get(s));
+            // }
         }
-        
+        answer[0]++;
+        answer[1]++;
         return answer;
     }
 }
