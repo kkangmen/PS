cdb5226de959d91b8ad7fb25c2626cd34fd4170e
김강민 <122205332+kkangmen@users.noreply.github.com>
2026-09-11 10:32:05 +0900

    [level 3] Title: 섬 연결하기, Time: 2.07 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -4,38 +4,44 @@ class Solution {
     
     int[] parent;
     
+    public void union(int node1, int node2){
+        int parent1 = findParent(node1);
+        int parent2 = findParent(node2);
+        
+        if (parent1 < parent2){
+            parent[parent2] = parent1;
+        } else {
+            parent[parent1] = parent2;
+        }
+    }
+    
     public int findParent(int node){
         if (parent[node] == node){
             return node;
-        }    
+        }
         return findParent(parent[node]);
-    }
-    
-    public void union(int a, int b){
-        int parent1 = findParent(a);
-        int parent2 = findParent(b);
-        
-        parent[parent2] = parent1;
     }
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
+        // init
         parent = new int[n];
         for (int i = 0; i < n; i++){
             parent[i] = i;
         }
         
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
-            
+        // sort
+        Arrays.sort(costs, (o1, o2) -> {
+            return o1[2] - o2[2];
+        });
+        
         for (int[] cost : costs){
-            // 부모가 다르면 간선 추가 후 통합
             if (findParent(cost[0]) != findParent(cost[1])){
-                answer += cost[2];
                 union(cost[0], cost[1]);
+                answer += cost[2];
             }
         }
-        
         return answer;
     }
 }
